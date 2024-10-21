7e6aa44bf01edf27bfea342548a393dbd7581598
rlvuddl1234 <rlvuddl1234@naver.com>
2024-10-21 19:28:09 +0900

    feat : 환경 세팅

---
+++ Final_Projcet/src/main/java/com/example/final_projcet/FinalProjcetApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_projcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjcetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjcetApplication.class, args);
+	}
+
+}
---
+++ Final_Projcet/src/test/java/com/example/final_projcet/FinalProjcetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_projcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjcetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
